95e789ab51286835cd904711703807dd3af2624e
lmw1663 <lmw1663@naver.com>
2024-03-26 23:40:28 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    length = len(s)
+    middle_index = length // 2
+    
+    if length % 2 == 1:
+        return s[middle_index]
+    else:
+        return s[middle_index - 1 : middle_index + 1]
